5df9b8c277c0601d3d6c9c7c2b8d29bd7f500fa9
SSSwetha25 <144270599+SSSwetha25@users.noreply.github.com>
2025-07-24 23:47:25 +0530

    header

--- client/src/assets/assets.jsx
+++ client/src/assets/assets.jsx
@@ -1,5 +1,8 @@
 import blog from './blog.png'
-
+import arrow from './arrow.png'
+import gradientbg from './gradientbg.jpg'
 export const assets={
-    blog
+    blog,
+    arrow,
+    gradientbg
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { assets } from '../assets/assets'
+import React from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
 const Navbar = () => {
-  return (
-    <div className='flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 cursor-pointer'>
-        <img src={assets.blog} alt="" className='w-32 sm:w-44' />
-        <button>Login 
-            <img src={assets.arrow} className='w-32' alt="arrows" />
-        </button>
-    </div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Navbar
+  return (
+    <div className="flex justify-between items-center py-5 mx-8 sm:mx-20 xl:mx-32 ">
+      <img
+        onClick={() => navigate("/")}
+        src={assets.blog}
+        alt=""
+        className="w-20 sm:w-15 cursor-pointer"
+      />
+      <button
+        onClick={() => navigate('/admin')}
+        className="flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5"
+      >
+        Login
+        <img src={assets.arrow} className="w-7" alt="arrows" />
+      </button>
+    </div>
+  );
+};
+
+export default Navbar;
