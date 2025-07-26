ad14121f0ea536277536442909c5bf5772e417ea
SSSwetha25 <144270599+SSSwetha25@users.noreply.github.com>
2025-07-26 22:17:51 +0530

    header enhancement

--- client/src/assets/assets.jsx
+++ client/src/assets/assets.jsx
@@ -1,10 +1,12 @@
 import blog from './blog.png'
 import arrow from './arrow.png'
 import gradientbg from './gradientbg.jpg'
+import star_icon from './star_icon.png'
 export const assets={
     blog,
     arrow,
-    gradientbg
+    gradientbg,
+    star_icon
 }
 
 export const blogCategories=['All','Technology','Startup','Lifestyle','Finance']
