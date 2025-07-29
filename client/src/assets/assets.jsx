8d2caedadfa16f7ac1b070c7cf5bb8f29deed403
SSSwetha25 <144270599+SSSwetha25@users.noreply.github.com>
2025-07-29 20:46:41 +0530

    Newsletter

--- client/src/assets/assets.jsx
+++ client/src/assets/assets.jsx
@@ -2,6 +2,7 @@ import blog from './blog.png'
 import arrow from './arrow.png'
 import gradientbg from './gradientbg.jpg'
 import star_icon from './star_icon.png'
+
 export const assets={
     blog,
     arrow,
