ed5c62ad0eb1ae5abb699d8358808e6cb5b360c1
SSSwetha25 <144270599+SSSwetha25@users.noreply.github.com>
2025-08-02 23:08:19 +0530

    Blog page navbar

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { blog_data } from '../assets/assets'
+import Navbar from '../components/Navbar'
 
 const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
+  const {id}=useParams()
+
+  const [data, setData]=useState(null)
+
+  const fetchBlogData= async ()=>{
+    const data= blog_data.find((item=>item._id===id))
+    setData(data)
+  }
+
+  useEffect(()=>{
+    fetchBlogData()
+  },[])
+
+  return data ? (
+    <div className="min-h-screen bg-gradient-to-br from-purple-50 to-white">
+<Navbar />
+      <div 
+      // className='backdrop-blur-sm bg-white/80 rounded-xl shadow-md p-8 mx-auto max-w-2xl mt-12'
+      >
+
+      </div>
+      <div></div>
+
+    </div>
+  ) : <div>Loading...</div>
 }
 
 export default Blog
